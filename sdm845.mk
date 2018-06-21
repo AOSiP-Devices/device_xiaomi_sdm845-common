79fb8d54f9e8e97968f1340a52fd29f44c7ab2bd
Michael Wright <michaelwr@google.com>
2018-06-21 02:38:28 +0100

    sdm845-common: Update power HAL to version 1.3
    This adds support for the EXPENSIVE_RENDERING power hint.
    
    BUG: 110112323
    Test: adb shell cat /sys/class/kgsl/kgsl-3d0/devfreq/cur_freq
          Verified GPU frequency reset by adb shell pkill surfaceflinger and system_server
    Change-Id: Ia1f6e99b67c5e88b610412ad30f152d241095a48
    Merged-In: Ia1f6e99b67c5e88b610412ad30f152d241095a48
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -225,7 +225,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service.xiaomi_sdm845-libperfmgr
+    android.hardware.power@1.3-service.xiaomi_sdm845-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
