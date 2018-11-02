e9dd0cd68613f8f115c34e7b3fe0606ad654bab6
argraur <argraur@gmail.com>
2018-11-02 12:47:48 +0000

    sdm845-common: set ro.sf.lcd_density (DPI) to 420
    Change-Id: I3fc7ddcd6275ded3c34323898052565b1e981142
    Signed-off-by: argraur <argraur@gmail.com>

--- common-props.mk
+++ common-props.mk
@@ -77,3 +77,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0
+
+# DPI
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=420
