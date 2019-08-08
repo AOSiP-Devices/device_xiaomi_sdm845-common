f2b282f6544d69b508cc1e171371447a58960b38
TheScarastic <warabhishek@gmail.com>
2019-08-08 12:27:03 +0100

    sdm845-common: Support DT2W in the new PowerHAL
    Change-Id: Icfdf8cc1340b493dab66c1fe56763e657efd5af9
    Co-authored-by: Bruno Martins <bgcngm@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

---
+++ power-libperfmgr/Android.mk
@@ -0,0 +1,39 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+ifneq ($(TARGET_TAP_TO_WAKE_NODE),)
+    LOCAL_CFLAGS += -DTAP_TO_WAKE_NODE=\"$(TARGET_TAP_TO_WAKE_NODE)\"
+endif
+
+LOCAL_MODULE := android.hardware.power@1.2-service.xiaomi_sdm845-libperfmgr
+LOCAL_INIT_RC := android.hardware.power@1.2-service.xiaomi_sdm845-libperfmgr.rc
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_VENDOR_MODULE := true
+
+LOCAL_SRC_FILES := \
+    InteractionHandler.cpp \
+    Power.cpp \
+    power-helper.c \
+    service.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    android.hardware.power@1.0 \
+    android.hardware.power@1.1 \
+    android.hardware.power@1.2 \
+    libbase \
+    libcutils \
+    libhidlbase \
+    libhidltransport \
+    liblog \
+    libutils \
+    libperfmgr
+
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
+LOCAL_HEADER_LIBRARIES += libhardware_headers
+
+LOCAL_CFLAGS += -Werror -Wall
+
+include $(BUILD_EXECUTABLE)
